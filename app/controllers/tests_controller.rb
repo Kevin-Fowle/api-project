460eccd16d787295ba96dc75d8b5f576d768e910
bplindgren <bplindgren91@gmail.com>
2016-05-19 17:14:20 -0500

    Remove traitify

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,15 +1,3 @@
-require 'httparty'
 get '/tests' do
-
-  @new_test_id = HTTParty.get("https://api-sandbox.traitify.com/v1/assessments?temp_key=i0fbrjancasvgcvlep3cf3d15j", { "Content-Type" => "application/json", "deck_id" => "career-deck" });
-  p "========="
-  p @new_test_id.parsed_response
-  p "========="
   erb :'tests/index'
-end
-
-get '/temp_key' do
-  p "temp key"
-  p params
-
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -3,13 +3,4 @@ class Test < ActiveRecord::Base
   belongs_to :user
   belongs_to :pokemon
 
-  def self.take_test
-    # sends a request to take the test
-    @request = HTTP.get("https://api-sandbox.traitify.com/v1/assessments?temp_key=i0fbrjancasvgcvlep3cf3d15j", { "Content-Type" => "application/json", "deck_id" => "career-deck" });
-
-  end
-
-  def self.get_test_result
-    # After user takes a test, this is where we deal with the result
-  end
 end
