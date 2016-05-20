b1d90fb261ee40ee4353ba7d843d17ef80c81ebe
bplindgren <bplindgren91@gmail.com>
2016-05-19 20:01:21 -0500

    Property type method

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,3 +1,12 @@
 get '/tests' do
   erb :'tests/index'
 end
+
+post '/tests' do
+  p params
+  pokemon_hash = get_pokemon(params)
+  p pokemon_hash
+  # @get_pokemon = Pokemon.find_by(:property => pokemon_hash)
+  # p @get_pokemon
+  erb :'show'
+end
